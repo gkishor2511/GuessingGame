20f8dd5ab9a6316cd4ea1e6b2a2784a42bf00322
G Kishor Kumar Reddy <100801460+gkishor2511@users.noreply.github.com>
2024-08-20 10:58:21 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import QuizApp from './tic/components/QuizApp';
+import QuessingGame from './components/GuessingGame'
+
+function App() {
+    return (
+        <div className="App">
+            <QuizApp />
+            <QuessingGame />
+        
+
+        </div>
+    );
+}
+
+export default App;
+
+
+
---
+++ src/main.jsx
@@ -0,0 +1,30 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App'; // Ensure the path is correct
+// import './index.css'; // Ensure Tailwind CSS is imported
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
